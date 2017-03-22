cbc8e654bcc76dc5d60bce04fa31b24ce7d08195
Yuri Bett <yuri@dynamix.com.br>
2017-03-22 12:44:39 -0300

    Adding list model

--- client/list/list.js
+++ client/list/list.js
@@ -1,8 +1,5 @@
 Template.list.helpers({
     tasks : function() {
-        return [
-            {'name' : 'Test 1'},
-            {'name' : 'Test 2'}
-        ];
+        return Tasks.find({});
     }
 });
