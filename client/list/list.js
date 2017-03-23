3cded2423ec47a11ff86be09382ab3de4e6d6c24
Yuri Bett <yuribett@gmail.com>
2017-03-22 21:55:38 -0300

    Removing task

--- client/list/list.js
+++ client/list/list.js
@@ -8,3 +8,12 @@ Template.list.helpers({
         return moment(this.date).format('DD/MM/YYYY HH:mm');
     }
 });
+
+Template.list.events({
+    
+    'click button' : function (event, template) {
+        let task = this;
+        Tasks.remove({ _id : task._id });
+    }
+
+});
